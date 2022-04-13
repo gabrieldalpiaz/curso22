9ce26848796e9fce6ca6b2644b673c1fc0de4492
gabrieldalpiaz <gabrieldalpiaz19@gmail.com>
2022-04-12 22:10:04 -0300

    variaveis PHP

--- usuario.php
+++ usuario.php
@@ -1,4 +1,36 @@
+<form action="usuario.php" method="post">
+    Nome:  <input type="text" name="personal[name]" /><br />
+    Email: <input type="text" name="personal[email]" /><br />
+    <input type="submit" value="Enviar dados!" />
+    </form>
+
 <?php
 
-echo "Sou o usuario!";
+echo "Teste de números";
 
+echo "<br>";
+echo "<br>";
+
+$numero = 95;
+$resto = 0;
+
+$resto = $numero % 2;
+
+if ($resto == 0){
+    echo "O número {$numero} é par";
+}
+else {
+    echo "O número {$numero} é impar";
+}
+
+$soma = 0;
+$valor1 = 10;
+$valor2 = 20;
+
+$soma = $valor1 + $valor2;
+
+echo "<br><br> A soma dos números {$valor1} + {$valor2} é igual a: {$soma} ";
+
+echo "<br><br>"; 
+
+$_POST
